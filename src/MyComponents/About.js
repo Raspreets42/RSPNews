4d799fe57b4fb6cf5edf078c6802b31d4d70e146
Raspreets42 <raspreets89@gmail.com>
2021-06-17 18:51:56 +0530

    Second commit

--- src/MyComponents/About.js
+++ src/MyComponents/About.js
@@ -9,11 +9,10 @@ export default function Header() {
                 <hr />
                 <h5 className="text-center">"News is a delicate balance of serving public good and private gain."</h5>
                 <br />
-                <p>It's Now become very neccessary to have a good knowledge of what is happening in the world day by day.
-            <b><i> The RSP News</i></b> presents top 20 News of the day. <b><i> The RSP News Chennel</i></b> launched in June' 2021. The channel is designed using the ReactJs Framework and is operated by <b><i> Mr. Raspreet Singh Pasrija.</i></b></p>
+                <p>It's Now become very neccessary to have a good knowledge of what is happening in the world day by day.<b><i> The RSP News</i></b> presents top 20 News of the day. <b><i> The RSP News Chennel</i></b> launched in June' 2021. The channel is designed using the ReactJs Framework and is operated by <b><i> Mr. Raspreet Singh Pasrija.</i></b></p>
                 <br />
                 <h6>Mr. Raspreet Singh Pasrija words :</h6>
-                <p><b><i> The RSP News Chennel</i></b> is the first API based Project. I designed <b><i> The RSP News Chennel</i></b> in a single day only and one of my friend <b><i> Aman Mahore </i></b> helped me when I'm facing some issues with my project <b><i> The RSP News Chennel.</i></b> This is just an start to the way of becoming an Web Developer. </p>
+                <p><b><i> The RSP News Chennel</i></b> is the first API based Project. I designed <b><i> The RSP News Chennel</i></b> in 2 days only and one of my friend <b><i> Aman Mahore </i></b> helped me when I'm facing some issues with my project <b><i> The RSP News Chennel.</i></b> This is just an start to the way of becoming an Web Developer. </p>
                 <br />
                 <p><b><i> About Me :</i></b><br /> I'm a <b><i> BE 3<sup>rd</sup> year student</i></b> studying <b>Computer Science Engineering</b> at <b>Rungta College of Engineering and Technology , Bhilai.</b> I like Programmings and will soon start an Bloging on the Programmings. </p>
 
--- src/MyComponents/News.js
+++ src/MyComponents/News.js
@@ -4,39 +4,45 @@ import "../App.css";
 export default function News() {
     const [data, setdata] = useState([])
 
+    const getLatestNews = async () => {
+        const response = await fetch(`https://newsapi.org/v2/top-headlines?country=in&apiKey=7831f4e8eb0c43c9be6c54da15445650`);
+        const actualData = await response.json();
+        console.log(actualData.articles);
+        setdata(actualData.articles);
+
+    }
     useEffect(() => {
-        fetch(`https://newsapi.org/v2/top-headlines?country=in&apiKey=7831f4e8eb0c43c9be6c54da15445650`)
-            .then((result) => {
-                result.json().then((response) => {
-                    setdata(response.articles)
-                })
-            })
+        getLatestNews();
     }, [])
     return (
-        <div>
+        <>
             <div className="container text-center my-4">
                 <h1>Welcome to <span className="badge rounded-pill bg-success">RSP News Channel</span> </h1>
             </div>
             <div className="container text-center">
                 <div className="accordion" id="NewsAccordion">
-                    {data.map((news, index) =>
-                        <div className="accordion-item" key={index}>
-                            <h2 className="accordion-header" id={"heading" + index}>
-                                <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target={"#collapse" + index} aria-expanded="true" aria-controls={"collapse" + index}>
-                                    <b><i>{index + 1}:- </i></b> {news.title}
-                                </button>
-                            </h2>
-                            <div id={"collapse" + index} className="accordion-collapse collapse " aria-labelledby={"heading" + index} data-bs-parent="#NewsAccordion">
-                                <div className="accordion-body">
-                                    <strong>{news.content}</strong>
-                                    <a href={news.url} target="_blank" rel="noreferrer">Read more.</a>
+                    {
+                        data.map((news, index) => {
+                            return (
+                                <div className="accordion-item" key={index}>
+                                    <h2 className="accordion-header" id={"heading" + index}>
+                                        <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target={"#collapse" + index} aria-expanded="true" aria-controls={"collapse" + index}>
+                                            <b><i>{index + 1}:- </i></b> {news.title}
+                                        </button>
+                                    </h2>
+                                    <div id={"collapse" + index} className="accordion-collapse collapse " aria-labelledby={"heading" + index} data-bs-parent="#NewsAccordion">
+                                        <div className="accordion-body">
+                                            <strong>{news.content}</strong>
+                                            <a href={news.url} target="_blank" rel="noreferrer">Read more.</a>
+                                        </div>
+                                        <p>Published At : {news["publishedAt"]}</p>
+                                    </div>
                                 </div>
-                                <p>Published At : {news["publishedAt"]}</p>
-                            </div>
-                        </div>
-                    )}
+                            )
+                        })
+                    }
                 </div>
             </div>
-        </div >
+        </>
     )
 }
